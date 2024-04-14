0864c924086e2a030715eafc31334a435681f61a
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2024-04-14 19:41:56 +0500

    created number gussing game

---
+++ main.js
@@ -0,0 +1,20 @@
+!#;
+import inquirer from "inquirer";
+// computer will gen a random number
+// user input for guess
+//compare user input
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "guess a number between 1-10",
+    },
+]);
+console.log(answers);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guess the right number");
+}
+else {
+    console.log("Sorry you guess wrong the number ");
+}
